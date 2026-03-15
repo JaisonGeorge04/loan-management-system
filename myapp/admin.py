aa25194b18f247abc3fd789b1a8ccea8545ef0fc
Jaison George <jaisongeorge699@gmail.com>
2026-03-15 17:35:20 +0530

    insert all the files

---
+++ myapp/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(Registration)
+admin.site.register(Manager)
+admin.site.register(Branch)
+admin.site.register(LoanApplication)
